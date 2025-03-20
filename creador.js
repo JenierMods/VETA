81f1cb265a47f5a346555e782c5d325dbf274f04
@JenierMods OFC <Jeniermods32@gmail.com>
2025-03-19 21:19:50 -0600

    Update creador.js

--- creador.js
+++ creador.js
@@ -1,4 +1,17 @@
- const creatorImage = document.querySelector('.creator-image');
+function resizeImage() {
+    // Obtener el valor del control deslizante
+    var size = document.getElementById("image-size").value;
+    
+    // Cambiar el tamaño de la imagen
+    var image = document.getElementById("creatorImage");
+    image.style.width = size + "px";
+    image.style.height = size + "px";
+    
+    // Mostrar el valor actual del control deslizante
+    document.getElementById("image-size-value").textContent = size + "px";
+}
+
+const creatorImage = document.querySelector('.creator-image');
 let isAnimatingOut = false;
 
 creatorImage.addEventListener('click', toggleAnimation);
@@ -17,14 +30,19 @@ function toggleAnimation() {
   }
 }
 
-        function openWhatsApp() {
+function openWhatsApp() {
   window.open('https://wa.me/+50577530939', '_blank');
-        }
+}
 
-        function openFacebook() {
-            window.open('https://www.facebook.com/share/1B7o3hPAr4/', '_blank');
-        }
-        
-        function openGithub() {
-            window.open('https://github.com/JenierMods', '_blank');
-        }
+function openFacebook() {
+    window.open('https://www.facebook.com/share/1B7o3hPAr4/', '_blank');
+}
+
+function openGithub() {
+    window.open('https://github.com/JenierMods', '_blank');
+}
+
+// Función para abrir la página de PayPal para donar
+function openPayPal() {
+    window.open('https://paypal.me/JenierMods?country.x=NI&locale.x=es_XC', '_blank');
+}
